74136f7a2e2acfb5a98841da4d455439948c7c61
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-08-22 10:55:54 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+  public static void main(String ar[])
+  {
+      int a,b,c;
+      a = 10;
+      b = 20;
+      c = a + b;
+      System.out.println("Sum = "+c);
+   }
+}
---
+++ Sum.java
@@ -0,0 +1,8 @@
+class Sum
+{
+  public static void main(String ar[])
+  {
+      System.out.print(ar[0]+" ");
+      System.out.print(ar[1]);
+  }
+}
