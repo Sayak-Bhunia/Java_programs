2260837e8b3c5f28ed3c76a35d4ab43836bd6ad7
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-08-22 10:57:04 +0530

    Update SumofN.java

--- SumofN.java
+++ SumofN.java
@@ -3,9 +3,9 @@ class SumofN
   public static void main(String ar[])
   {
       int sum = 0;
-      for(int i=0;i<ar.length;i++) 
+      for(int i=0;i<ar.length;i++) // here .length is a variable defined array class
       {
-          sum = sum + Integer.parseInt(ar[i]);
+          sum = sum + Integer.parseInt(ar[i]); // string -> int
       }
       System.out.println("Sum = "+sum);
   }
