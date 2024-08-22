c21241b852f58061ae183f0f43db30130ab7af9d
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-08-22 11:18:54 +0530

    Create scanner_class.java

---
+++ scanner_class.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sum1
+{
+  public static void main(String ar[])
+  {
+      Scanner sc = new Scanner(System.in); // sc is the object of the scanner class
+      int a,b,c;
+      System.out.println("Enter 2  nos");
+      a = sc.nextInt();
+      b = sc.nextInt();
+      c = a + b;
+      System.out.println("Sum = "+c);
+  }
+}
