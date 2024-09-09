15049688eb23ee3f6b9f31d9e1624eb20197f507
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-09-09 11:33:55 +0530

    Create static keyword method variable.java

---
+++ oops concept progs of theory/static keyword method variable.java
@@ -0,0 +1,11 @@
+class Demo {
+    static int a = 10;
+    static int display() {
+        return a;
+    }
+
+    public static void main(String[] args) {
+       System.out.println("a = "+ Demo.display());
+    }
+}
+
