d295d015f4938e7f9176825ce1a3c5b17554c492
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-11-21 17:45:10 +0530

    Update area method overload.java

--- area method overload.java
+++ area method overload.java
@@ -1,20 +1,19 @@
-public class Main {
+import java.util.*;
+
+class Main {
     public int area(int a, int b) {
         return a*b;
     }
     public double area(double r) {
         return Math.PI*r*r;
     }
-    public double area(double b, double h) {
-        return 0.5*b*h;
+    public double area(double a, double b) {
+        return 0.5*a*b;
     }
     public static void main(String[] args) {
-        Main obj = new Main();
-        int rarea = obj.area(5, 4);
-        System.out.println("area of rectangle = "+rarea);
-        double carea = obj.area(5);
-        System.out.println("area of circle = "+carea);
-        double tarea = obj.area((float)5, (float)6);
-        System.out.println("area of triangle = "+tarea);
+       Main obj  = new Main();
+       System.out.println("Area of Reactangle = "+obj.area(5, 6));
+       System.out.println("Area of Circle = "+obj.area(5.0));
+       System.out.println("Area of Triangle = "+obj.area(5.0, 6.0));
     }
 }
