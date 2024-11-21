ae8aec5cbb579ca8c1722ae330911dada448ba1c
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-11-21 17:37:05 +0530

    Update Rect area.java

--- Rect area.java
+++ Rect area.java
@@ -1,22 +1,19 @@
 import java.util.*;
-class Rect {
-    int length, breadth;
-    
-    Rect(int l, int b) {
-        length = l;
-        breadth = b;
-    }
-    
-    void area() {
-        System.out.println("area = " + length*breadth);
-    }
 
+class Main {
+    int l, r;
+    Main(int l, int r) {
+        this.l = l;
+        this.r = r;
+    }
+    int area(){
+        return l*r;
+    }
     public static void main(String[] args) {
-        int a, b;
         Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-        b = sc.nextInt();
-        Rect r = new Rect(a, b);
-        r.area();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        Main r = new Main(a, b);
+        System.out.println("Area = "+r.area());
     }
 }
