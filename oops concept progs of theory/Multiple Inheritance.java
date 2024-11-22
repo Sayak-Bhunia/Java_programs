7623437a75a4af65d3b8d3c776e386295a3271a5
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-11-22 18:03:59 +0530

    Update Multiple Inheritance.java

--- oops concept progs of theory/Multiple Inheritance.java
+++ oops concept progs of theory/Multiple Inheritance.java
@@ -6,16 +6,15 @@ interface B {
     void fun();
 }
 
-class C implements A, B {
+class Main implements A, B {
 
     public void fun() {
-        System.out.println("Multiple Inheritance using intercepts");
+        System.out.println("Multiple Inheritance using interfaces");
     }
-    
+
     public static void main(String args[]) {
-        C c = new C();
+        Main c = new Main();
         c.fun();
     }
 }
-
 // In java multiple inheritance is not doable coz the compilar gets confused if the two or more classes havce a method with the same name in all of those classes so to solve this java allows intercepts to handle multiple inheritance using implements but not extends unlike normal inheritance in classes in case of intercepts the functions/methods cant have a body so the common dunctoin has to defined in the body of the main class where the intercepts are being implemented 
