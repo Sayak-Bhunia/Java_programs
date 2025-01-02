ac2085cf9f291914accec8eaa6e273fb8be18bdf
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-01-02 14:59:32 +0530

    Update method_overriding.java

--- oops concept progs of theory/method_overriding.java
+++ oops concept progs of theory/method_overriding.java
@@ -1,21 +1,31 @@
-import java.util.*;
-
-class Parent {
-    void show() {
-        System.out.println("Parent class");
+class Animal {
+    void sound() {
+        System.out.println("This is a generic animal sound");
     }
 }
 
-class Child extends Parent {
-    @Override void show() {
-        super.show();
-        System.out.println("Child class");
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("The dog barks: Woof Woof!");
     }
 }
 
-class Main {
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("The cat meows: Meow Meow!");
+    }
+}
+
+public class MethodOverridingDemo {
     public static void main(String[] args) {
-        Parent obj = new Child();
-        obj.show();
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.sound();
+        myDog.sound();
+        myCat.sound();
     }
 }
