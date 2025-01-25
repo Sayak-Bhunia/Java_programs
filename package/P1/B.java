867b908a9ab302556e39bdfa75b18a11d7c2892c
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-01-25 13:27:19 +0530

    Create B.java

---
+++ package/P1/B.java
@@ -0,0 +1,12 @@
+package P1;
+
+public class B {
+    public static void main(String[] args) {
+        A obj =  new A();
+	System.out.println("class B");
+        System.out.println("no private");
+        System.out.println(obj.b);
+        System.out.println(obj.c);
+        System.out.println(obj.d);
+    }
+}
